46a95088ee97c46409f9a547d46747f41db045be
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-22 21:49:12 +0100

    feat: make display scrollable

--- app/src/main/java/com/mdzeviatau/calculator/ui/components/Display.kt
+++ app/src/main/java/com/mdzeviatau/calculator/ui/components/Display.kt
@@ -1,10 +1,13 @@
 package com.mdzeviatau.calculator.ui.components
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,11 +15,20 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun Display(modifier: Modifier = Modifier, text: String) {
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(text) {
+        scrollState.animateScrollTo(scrollState.maxValue)
+    }
+
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .padding(30.dp), contentAlignment = Alignment.BottomEnd
+            .padding(30.dp)
+            .horizontalScroll(
+                state = scrollState
+            ), contentAlignment = Alignment.BottomEnd
     ) {
-        Text(text = text.ifEmpty { "0" }, fontSize = 50.sp)
+        Text(text = text.ifEmpty { "0" }, fontSize = 50.sp, maxLines = 1)
     }
 }
